9323e21a36db1f3513832e5242d82b798f0607c3
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2022-12-06 22:26:38 +0100

    Update settings.lua

--- GAMEPLAY/settings.lua
+++ GAMEPLAY/settings.lua
@@ -25,19 +25,19 @@ function setMapData(seed)
 
     if seed.difficulty == "easy" then 
         rates.mines = 0.15
-        rates.gold  = 0.15
-        rates.bonus = 0
+        rates.gold  = 0.12
+        rates.bonus = 4
     end
 
     if seed.difficulty == "normal" then 
-        rates.mines = 0.15
-        rates.gold  = 0.07
+        rates.mines = 0.20
+        rates.gold  = 0.15
         rates.bonus = 3
     end
 
     if seed.difficulty == "hard" then 
-        rates.mines = 0.20
-        rates.gold  = 0.10
+        rates.mines = 0.25
+        rates.gold  = 0.20
         rates.bonus = 0
     end
 
@@ -46,7 +46,6 @@ function setMapData(seed)
     mapData.gold         = math.floor(mapData.totalSquares * rates.gold)
     mapData.sweepers.max = mapData.mines + mapData.gold + rates.bonus
 
-  
 
     if seed.mapMode == "custom" then
         mapData.rows         = ui.menuScreen.custom.customSize.rows.gauge.number
@@ -55,11 +54,13 @@ function setMapData(seed)
         mapData.gold         = ui.menuScreen.custom.customGold.gauge.number
         mapData.sweepers.max = mapData.mines + mapData.gold + 
                                 ui.menuScreen.custom.customSweepers.gauge.number
+
         end
         
     mapData.sweepers.required = math.floor((mapData.totalSquares - mapData.mines - 
                                             mapData.gold) / mapData.sweepers.max)
     mapData.sweepers.limit    = mapData.sweepers.max * mapData.sweepers.required 
+
 
     if seed.gameMode == "classic" then
         mapData.gold              = 0
@@ -68,11 +69,16 @@ function setMapData(seed)
         mapData.sweepers.limit    = 0
     end
 
-    print(mapData.mines)
-    print(mapData.gold)
-    print(mapData.sweepers.max)
-    print(mapData.sweepers.required)
-    print(mapData.sweepers.limit)
+    print("!!!!!!!!!!!!!!!!!NEW GAME!!!!!!!!!!!!!!!!")
+    print("Rows          : " ..mapData.rows)
+    print("Columns       : " ..mapData.columns)
+    print("Total Squares : " ..mapData.totalSquares)
+    print("Mines         : " ..mapData.mines)
+    print("Gold          : " ..mapData.gold)
+    print("Sweepers:")
+    print("     Max      : " ..mapData.sweepers.max)
+    print("     Required : " ..mapData.sweepers.required)
+    print("     Limit    : " ..mapData.sweepers.limit)
         
     return mapData
 end
